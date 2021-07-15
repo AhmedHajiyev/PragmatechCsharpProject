f86afa34342f94d813b75c97eac184ffaa4e6c82
AhmedHajiyev <hajiyev.ahmed01@gmail.com>
2021-07-16 00:58:19 +0400

    week06 task done

--- Week06/Task1/Form1.Designer.cs
+++ Week06/Task1/Form1.Designer.cs
@@ -48,6 +48,9 @@
             this.label6 = new System.Windows.Forms.Label();
             this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
             this.SuspendLayout();
@@ -160,7 +163,7 @@
             this.button1.Location = new System.Drawing.Point(107, 251);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 10;
+            this.button1.TabIndex = 14;
             this.button1.Text = "Sifaris et";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
@@ -179,6 +182,7 @@
             this.listView1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
             this.listView1.Location = new System.Drawing.Point(346, 24);
             this.listView1.Name = "listView1";
+            this.listView1.ShowItemToolTips = true;
             this.listView1.Size = new System.Drawing.Size(364, 97);
             this.listView1.TabIndex = 11;
             this.listView1.UseCompatibleStateImageBehavior = false;
@@ -222,7 +226,7 @@
             this.numericUpDown2.Location = new System.Drawing.Point(108, 179);
             this.numericUpDown2.Name = "numericUpDown2";
             this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
-            this.numericUpDown2.TabIndex = 13;
+            this.numericUpDown2.TabIndex = 10;
             // 
             // dateTimePicker1
             // 
@@ -230,13 +234,43 @@
             this.dateTimePicker1.Location = new System.Drawing.Point(108, 213);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(120, 20);
-            this.dateTimePicker1.TabIndex = 14;
+            this.dateTimePicker1.TabIndex = 13;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(346, 137);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(243, 20);
+            this.textBox1.TabIndex = 15;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(595, 137);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 20);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Axtar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(716, 24);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "Sil";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.numericUpDown2);
             this.Controls.Add(this.label6);
@@ -282,6 +316,9 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.NumericUpDown numericUpDown2;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Week06/Task1/Form1.cs
+++ Week06/Task1/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections;
 
 namespace Task1
 {
@@ -18,19 +19,18 @@ namespace Task1
         {
             InitializeComponent();
         }
-
         public class Click
         {
-            public string category { get; set; }
-            public string product { get; set; }
-            public string weight { get; set; }
-            public string delivery { get; set; }
-            public string price { get; set; }
-            public DateTime orderedDate { get; set; }
-            
+            public string category;
+            public string product;
+            public string weight;
+            public string delivery;
+            public string price;
+            public string orderedDate;
         }
 
-        public Click ClickCreate()
+
+        public Click Sifaris()
         {
             Click e = new Click
             {
@@ -39,10 +39,16 @@ namespace Task1
                 weight = Convert.ToString(numericUpDown1.Value),
                 delivery = comboBox3.Text,
                 price = Convert.ToString(numericUpDown2.Value),
-                orderedDate = dateTimePicker1.Value
+                orderedDate = dateTimePicker1.Value.ToShortDateString()
 
             };
+            return e;
         }
+
+       
+
+        
+
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
@@ -85,13 +91,78 @@ namespace Task1
                     break;
             }
         }
-        
-       
+
         private void button1_Click(object sender, EventArgs e)
         {
-           
+
+            Click newSifaris = Sifaris();
+            ListViewItem item = new ListViewItem();
+            item.Text = newSifaris.category;
+            item.SubItems.Add(newSifaris.product);
+            item.SubItems.Add(newSifaris.weight);
+            item.SubItems.Add(newSifaris.delivery);
+            item.SubItems.Add(newSifaris.price);
+            item.SubItems.Add(newSifaris.orderedDate);
+
+            if (newSifaris.category != null && newSifaris.product != null && newSifaris.weight != "0" && newSifaris.delivery != null && newSifaris.price != "0")
+            {
+                listView1.Items.Add(item);
+                comboBox1.SelectedIndex = -1;
+                comboBox2.SelectedIndex = -1 ;
+                comboBox3.SelectedIndex = -1;
+                numericUpDown1.Value = 0;
+                numericUpDown2.Value = 0;
+                dateTimePicker1.Value = DateTime.Now;
+            }
+            else
+            {
+                MessageBox.Show("Xanalari doldurun");
+            }
+
+
             
+
         }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string search = textBox1.Text.Trim();
+            textBox1.Clear();
+            var arlist = new ArrayList();
+            arlist.Add(listView1);
+
+            for (int i = 0; i < listView1.Items.Count; i++)
+            {
+                if (listView1.Items[i].SubItems[1].Text == search )
+                {
+                    //for (int a = listView1.Items.Count - 1; a > 0; a--)
+                    //{
+                       //if (!listView1.Items[i].ToString().Contains(search))
+                        //{
+                            //listView1.Items.Remove(listView1.Items[a]);
+                            MessageBox.Show("Bu mehsul var");
+
+                            
+                            
+                        //}
+                    //}
+
+                }
+                else
+                {
+                    MessageBox.Show("Bu mehsul yoxdur");
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            for (int i = listView1.SelectedItems.Count-1; i >= 0; i--)
+            {
+                listView1.Items[i].Remove();
+            }
+        }
+
+        
     }
 }
